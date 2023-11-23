9b37752c7fca17cc3fb75c1fe86db2b3dbb67316
HeaX <heax@moonsama.com>
2023-11-23 14:32:57 +0100

    feat: added generic entity component interface

--- Runtime/Interfaces/IEntityComponent.cs
+++ Runtime/Interfaces/IEntityComponent.cs
@@ -1,4 +1,5 @@
 ﻿using GameKit.Entities;
+using UnityEngine;
 
 namespace GameKit
 {
@@ -6,5 +7,27 @@ namespace GameKit
     {
         void Initialize(EntityController controller);
         void ApplyEntity(IEntity entity);
+    }
+
+    public interface IEntityComponent<in T> where T : IEntity
+    {
+        sealed void ApplyEntity(IEntity entity)
+        {
+            if (entity == null)
+            {
+                ApplyEntity(default);
+                return;
+            }
+
+            if (entity is not T t)
+            {
+                Debug.LogWarning("Cannot assign entity of type " + entity.GetType().Name + " as " + typeof(T).Name);
+                return;
+            }
+
+            ApplyEntity(t);
+        }
+
+        void ApplyEntity(T entity);
     }
 }
