e7cb4b4d83ecb4259e2bab8416d853b94aee5adf
HeaX <heax@moonsama.com>
2023-11-24 13:46:56 +0100

    feat: added interface for animations

---
+++ Runtime/Components/Entities/Aspects/UnityEntityAnimation.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace GameKit.Entities
+{
+    [RequireComponent(typeof(Animator))]
+    public class UnityEntityAnimation : MonoBehaviour, IEntityAnimation
+    {
+        [SerializeField] [HideInInspector] private Animator _animator;
+
+        public float animationCrossFade { get; set; } = 0.1f;
+
+        public void Initialize(EntityController controller)
+        {
+        }
+
+        public void ApplyEntity(IEntity entity)
+        {
+        }
+
+        public void Play(string animation, float transition)
+        {
+            _animator.CrossFadeInFixedTime(animation, transition);
+        }
+
+        public void Pause()
+        {
+            _animator.StopPlayback();
+        }
+
+        public void Resume()
+        {
+            _animator.StartPlayback();
+        }
+    }
+}
---
+++ Runtime/Interfaces/IEntityAnimation.cs
@@ -0,0 +1,11 @@
+﻿namespace GameKit
+{
+    public interface IEntityAnimation : IEntityComponent
+    {
+        public float animationCrossFade { get; set; }
+        void Play(string animation) => Play(animation, animationCrossFade);
+        void Play(string animation, float transition);
+        void Pause();
+        void Resume();
+    }
+}
