4ff3d8cb034ef821aa4427d4b3984e4f715b48cf
HeaX <heax@moonsama.com>
2023-12-16 16:17:08 +0100

    feat: added method to access data without creating it

--- Runtime/Interfaces/IEntity.cs
+++ Runtime/Interfaces/IEntity.cs
@@ -11,6 +11,13 @@ namespace GameKit
 
         /**
          * Get a metadata block of the specified type.
+         * If no block of the requested type is present, this returns <value>false</value>
+         * The returned data is mutable.
+         */
+        bool TryGetData<T>(out T data) where T : IEntityData, new();
+        
+        /**
+         * Get a metadata block of the specified type.
          * If no block of the requested type is present, a new one is created.
          * The returned data is mutable.
          */
--- Runtime/Models/Entity.cs
+++ Runtime/Models/Entity.cs
@@ -11,13 +11,19 @@ namespace GameKit
 
         private readonly HashSet<IEntityData> _dataObjects = new();
 
+        public bool TryGetData<T>(out T data) where T : IEntityData, new()
+        {
+            data = _dataObjects.OfType<T>().FirstOrDefault();
+            return data != null;
+        }
+
         public T GetData<T>() where T : IEntityData, new()
         {
             return _dataObjects.OfType<T>().DefaultIfEmpty(CreateMetadata<T>()).First();
         }
 
         public IEnumerable<T> GetAllData<T>() where T : IEntityData => _dataObjects.OfType<T>();
-        
+
         public IEnumerable<IEntityData> GetAllData() => _dataObjects;
 
         private T CreateMetadata<T>() where T : IEntityData, new()
