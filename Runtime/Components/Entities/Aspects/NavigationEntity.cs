c21965ae4aea918628590ee9c7da173670d71c3f
HeaX <heax@moonsama.com>
2023-12-18 11:52:06 +0100

    fix: fixed rotation handling

--- Runtime/Components/Entities/Aspects/NavigationEntity.cs
+++ Runtime/Components/Entities/Aspects/NavigationEntity.cs
@@ -5,13 +5,12 @@ namespace GameKit.Entities
     [RequireComponent(typeof(CharacterController))]
     public class NavigationEntity : MonoBehaviour, IEntityComponent
     {
-        public delegate void MoveEvent(Vector3 position, float yaw);
+        public delegate void MoveEvent(Vector3 position, Quaternion rotation);
 
         public event MoveEvent moved = delegate { };
 
         [SerializeField] [HideInInspector] private CharacterController _characterController;
 
-        private float _yaw;
         private Transform _transform;
         private bool _gravityAppliedThisTick = false;
         private bool _velocityUpdatedThisTick = false;
@@ -36,16 +35,9 @@ namespace GameKit.Entities
 
         public bool hasGravity { get; set; }
         public bool controlYaw { get; set; } = true;
+        public bool controlRotation { get; set; } = false;
 
-        public float yaw
-        {
-            get => _yaw;
-            set
-            {
-                _yaw = value;
-                rotation = Quaternion.Euler(0, value, 0);
-            }
-        }
+        public float yaw { get; private set; }
 
         private void OnEnable()
         {
@@ -71,14 +63,22 @@ namespace GameKit.Entities
         public void SetYawWithoutNotify(float yaw)
         {
             this.yaw = yaw;
+            rotation = Quaternion.Euler(0, yaw, 0);
             currentRotation = rotation;
             if (controlYaw) transform.localRotation = currentRotation;
+        }
+
+        public void SetRotationWithoutNotify(Quaternion rotation)
+        {
+            yaw = rotation.eulerAngles.y;
+            currentRotation = rotation;
+            if (controlRotation) transform.localRotation = currentRotation;
         }
 
         private void LateUpdate()
         {
             currentRotation = Quaternion.RotateTowards(currentRotation, rotation, Time.deltaTime * 1080);
-            if (controlYaw) transform.localRotation = currentRotation;
+            if (controlYaw || controlRotation) transform.localRotation = currentRotation;
             if (!_gravityAppliedThisTick && hasGravity)
             {
                 Move(new Vector3(0, -1, 0) * Time.deltaTime);
@@ -99,25 +99,29 @@ namespace GameKit.Entities
         /// </summary>
         /// <param name="motion">Attempted motion from the current position</param>
         public CollisionFlags Move(Vector3 motion) => Move(motion,
-            motion.sqrMagnitude > 0.000001f
-                ? motion.x > 0
-                    ? Vector3.Angle(Vector3.forward, motion.normalized)
-                    : 360 - Vector3.Angle(Vector3.forward, motion.normalized)
-                : yaw);
+            motion.sqrMagnitude > 0.0001f ? Quaternion.LookRotation(motion) : rotation);
 
         /// <summary>
         ///   Supplies the movement of a GameObject with an attached CharacterController component.
         /// </summary>
         /// <param name="motion">Attempted motion from the current position</param>
         /// <param name="yaw">Yaw in degrees where 0 points north, 90 points east, 180 points south and 270 points west</param>
-        public CollisionFlags Move(Vector3 motion, float yaw)
+        public CollisionFlags Move(Vector3 motion, float yaw) => Move(motion, Quaternion.Euler(0, yaw, 0));
+
+        /// <summary>
+        ///   Supplies the movement of a GameObject with an attached CharacterController component.
+        /// </summary>
+        /// <param name="motion">Attempted motion from the current position</param>
+        /// <param name="rotation">Rotation</param>
+        public CollisionFlags Move(Vector3 motion, Quaternion rotation)
         {
             if (hasGravity) motion.y -= 1;
             var flags = characterController.Move(motion);
-            this.yaw = yaw;
+            yaw = rotation.eulerAngles.y;
+            this.rotation = rotation;
             _gravityAppliedThisTick = true;
             UpdateVelocity();
-            moved(transform.position, yaw);
+            moved(transform.position, rotation);
             return flags;
         }
 
@@ -125,24 +129,31 @@ namespace GameKit.Entities
         ///   Moves the character with speed.
         /// </summary>
         /// <param name="speed">Speed per second</param>
-        public void SimpleMove(Vector3 speed) => SimpleMove(speed,
-            speed.sqrMagnitude > 0.0000001f
-                ? speed.x > 0
-                    ? Vector3.Angle(Vector3.forward, speed.normalized)
-                    : 360 - Vector3.Angle(Vector3.forward, speed.normalized)
-                : yaw);
+        public void SimpleMove(Vector3 speed) => SimpleMove(speed, controlRotation && speed.sqrMagnitude > 0.0001f
+            ? Quaternion.LookRotation(speed)
+            : rotation);
 
         /// <summary>
         ///   Moves the character with speed.
         /// </summary>
         /// <param name="speed">Speed per second</param>
         /// <param name="yaw">Yaw in degrees where 0 points north, 90 points east, 180 points south and 270 points west</param>
-        public void SimpleMove(Vector3 speed, float yaw)
+        public void SimpleMove(Vector3 speed, float yaw) => SimpleMove(speed, controlYaw
+            ? Quaternion.Euler(0, yaw, 0)
+            : rotation);
+
+        /// <summary>
+        ///   Moves the character with speed.
+        /// </summary>
+        /// <param name="speed">Speed per second</param>
+        /// <param name="rotation">Rotation</param>
+        public void SimpleMove(Vector3 speed, Quaternion rotation)
         {
             if (!characterController.SimpleMove(speed)) return;
-            this.yaw = yaw;
+            yaw = rotation.eulerAngles.y;
+            this.rotation = rotation;
             UpdateVelocity();
-            moved(transform.position, yaw);
+            moved(transform.position, rotation);
         }
 
         private Vector3 UpdateVelocity()
